316e46bf2c033d4b550e1d868bad953a5e4531a1
mrlzmtl <mluizamnogueira@gmail.com>
2024-05-03 15:04:34 -0300

    feat: Exercício 3

---
+++ ex3.js
@@ -0,0 +1,11 @@
+// Exercício 3: Ímpar ou par
+
+// Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+let number = parseInt(prompt("Digite um número: "))
+
+if (number%2 == 0) {
+    alert("Par")
+} else {
+    alert("Ímpar")
+}
