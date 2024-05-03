6daa1b93c012c7e370d8d86dc92f2aaa145ee8dd
mrlzmtl <mluizamnogueira@gmail.com>
2024-05-03 15:41:41 -0300

    feat: Exercício 6

---
+++ ex6.js
@@ -0,0 +1,14 @@
+// Exercício 6: Verificação de idade
+
+// Escreva um programa que solicita o ano de nascimento de uma pessoa e verifica se ela é maior de 18 anos. 
+// Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
+
+let ano = parseInt(prompt("Digite seu ano de nascimento:"))
+
+let idade = 2024-ano
+
+if (idade < 18) {
+    alert("Menor de idade")
+} else {
+    alert("Maior de idade")
+}
