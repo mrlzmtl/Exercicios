806c1c240e3b9da02d39d89ee1481c32244b4118
mrlzmtl <mluizamnogueira@gmail.com>
2024-05-07 16:55:31 -0300

    feat: Exercício 3

---
+++ for-while/exfor3.js
@@ -0,0 +1,6 @@
+// Faça um programa que imprima os números ímpares de 0 a 50;
+for (let contador = 0; contador <= 50 ; contador++) {
+    if (contador%2 != 0) {
+        console.log(contador);
+    }
+}
