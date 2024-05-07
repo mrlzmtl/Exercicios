b3d0f87749f37339aa6bc8587286c0cf51d451b7
mrlzmtl <mluizamnogueira@gmail.com>
2024-05-07 17:09:50 -0300

    feat: Exercício 4

---
+++ for-while/exfor4.js
@@ -0,0 +1,7 @@
+// Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+let num = parseInt(prompt("Digite um número: "))
+
+for (let tab = 1; tab <= 10; tab++) {
+    console.log(num*tab);
+}
