36cd47bbfc7ab720cebe5629c1d97803b20bfeb0
mrlzmtl <mluizamnogueira@gmail.com>
2024-05-08 16:13:04 -0300

    feat: Exercício 10

--- for-while/ex10.js
+++ for-while/ex10.js
@@ -1,6 +1,11 @@
 // Faça um programa que receba um número do usuário e continue
 // a pedir o número se o numero for diferente de 0.
-let num = parseInt(prompt("Digite um número"))
-while (num == 0) {
-    console.log("Seu número é 0");
+let num = parseInt(prompt("Digite um número: "))
+
+while (num != 0) {
+    let numb = parseInt(prompt("Digite um número novamente: "))
+    num = numb
+}
+if (num == 0) {
+    console.log("Seu número é 0!");
 }
