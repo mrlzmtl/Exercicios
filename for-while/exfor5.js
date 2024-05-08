1626962cbbcfa13aff9724cf30bde94aa28fc55b
mrlzmtl <mluizamnogueira@gmail.com>
2024-05-08 14:35:55 -0300

    feat: Exercício 5

--- for-while/exfor5.js
+++ for-while/exfor5.js
@@ -1,2 +1,11 @@
 // Imprimir os primeiros 10 números da sequência de Fibonacci.
+let num1 = 0
+let num2 = 1
+let num3 = 1
 
+for (let fibonacci = 0; fibonacci <= 10; fibonacci++){
+    console.log(num1);
+    num3 = num1 + num2
+    num1 = num2
+    num2 = num3
+}
