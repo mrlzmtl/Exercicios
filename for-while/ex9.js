b612eee8b08cbdd2dd42ff47e6a55a4dbfcbe232
mrlzmtl <mluizamnogueira@gmail.com>
2024-05-08 15:28:03 -0300

    feat: Exercício 9

---
+++ for-while/ex9.js
@@ -0,0 +1,8 @@
+// Faça um programa que receba um número do usuário e mostre
+// no console a contagem regressiva até 0.
+let a = parseInt(prompt("Digite um número aleatório: "))
+
+while (a > 0) {
+    console.log(a);
+    a--
+}
