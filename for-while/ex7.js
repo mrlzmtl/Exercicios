69ef21bb49ca38cea054d89de935090b131704a8
mrlzmtl <mluizamnogueira@gmail.com>
2024-05-09 15:18:43 -0300

    feat: Exercício 7

--- for-while/ex7.js
+++ for-while/ex7.js
@@ -1,9 +1,9 @@
 // Calcular a soma dos números de 1 a 100 utilizando a estrutura
 // while.
-let a = 0
+let a = 1
 let b = 0
  while (a<=100) {
-    a++
-    b += a
+    b += a;
+    a++;
     console.log(b);
 }
